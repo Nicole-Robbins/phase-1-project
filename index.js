bb75982251ec9184c0b41cad827b8a18f7a1819b
Nicole Robbins <nicole.robbins@att.net>
2022-11-18 17:55:41 -0600

    add listener 1

---
+++ index.js
@@ -0,0 +1,9 @@
+document.addEventListener('DOMContentLoaded', () => {
+//event listener #1: dom content loaded
+
+//event listener #2: choosing a task enter button 'submit'
+// will add a task to the appropriate category using if then and iteration to look at each category
+
+//event listener #3: delete a task 'click'
+// will remove a task from the field but not the dropdown list
+})
